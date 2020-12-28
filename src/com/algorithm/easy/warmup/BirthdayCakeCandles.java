668df4f64edc25c1d67af38921f578e435a9867d
lebcoco <leee24638@gmail.com>
2020-12-28 17:51:11 +0900

    Upload easy/warmup/BirthdayCakeCandles

---
+++ src/com/algorithm/easy/warmup/BirthdayCakeCandles.java
@@ -0,0 +1,64 @@
+package com.algorithm.easy.warmup;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultCk {
+
+  /*
+   * Complete the 'birthdayCakeCandles' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts INTEGER_ARRAY candles as parameter.
+   */
+
+  public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+    int count = 0;
+    int max = 0;
+    for(int i = 0; i < candles.size() ; i++)
+    {
+      int num = candles.get(i);
+      if(num > max)
+      {
+        max = num;
+        count = 1;
+      }else if(max == num)
+      {
+        count++;
+      }
+    }
+    return count;
+
+  }
+
+}
+public class BirthdayCakeCandles {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+    List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        .map(Integer::parseInt)
+        .collect(toList());
+
+    int result = ResultCk.birthdayCakeCandles(candles);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
