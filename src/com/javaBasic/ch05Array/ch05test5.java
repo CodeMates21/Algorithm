04f0cc7c564d4d9be6090a8a544342297713a9f5
lebcoco <leee24638@gmail.com>
2020-12-29 18:56:38 +0900

    Upload javaBasic ch05 Array

--- src/com/javaBasic/ch05Array/ch05test5.java
+++ src/com/javaBasic/ch05Array/ch05test5.java
@@ -1,6 +1,6 @@
 package com.javaBasic.ch05Array;
 
-public class ch05test05 {
+public class ch05test5 {
 
   public static void main(String[] args) {
     int[][] array = { { 95, 86 }, { 83, 92, 96 }, { 78, 83, 93, 87, 88 } };
